b2bdb76bc6811c7e80081c23f09cb8093e249cc7
JackyJqz <junqingz98@gmail.com>
2018-08-27 14:12:52 -0400

    commented CSRF protection

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.core import serializers
+from .models import *
 # Create your views here.
 
 
 def foo(request):
     print(request)
     return HttpResponse("hello from foo")
+
